b8c2fe14f91b9b28b6b7d1eb7d70d2fb40e40590
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2025-02-05 10:08:37 -0300

    uno

---
+++ main.js
@@ -0,0 +1,92 @@
+
+let diaEvento = new Date('March 22, 2025 21:30:00').getTime();
+let boton = document.querySelector("#btn")
+
+
+
+// diaEvento = new Date().getTime() + 1_000 * 20;
+
+let relojE = document.getElementById('reloj');
+
+const diaM = 86_400_000;
+const horaM = 3_600_000;
+const minutoM = 60_000;
+const segundoM = 1_000;
+const ejecutarReloj = setInterval(
+    () => {
+
+        let hoy = new Date().getTime();
+        let distancia = diaEvento - hoy;
+
+        let diaR = Math.trunc(distancia / diaM);
+
+        let horaR = Math.trunc((distancia % diaM) / horaM);
+
+        let minutoR = Math.trunc((distancia % horaM) / minutoM);
+
+        let segundoR = Math.trunc((distancia % minutoM) / segundoM);
+
+        if (distancia  < 1) {
+            clearInterval(ejecutarReloj);
+            diaEventoAhora();
+        }
+
+        pintarElementos(diaR, horaR, minutoR, segundoR);
+
+    }
+, 1000);
+
+let creDia = document.getElementById('cre-dia');
+let creHora = document.getElementById('cre-hora');
+let creMinuto = document.getElementById('cre-minuto');
+let creSegundo = document.getElementById('cre-segundo');
+function pintarElementos(dia, hora, minuto, segundo) {
+
+    segundo = (segundo < 10) ? '0' + segundo : segundo;
+
+    minuto = (minuto < 10) ? '0' + minuto : minuto;
+
+    hora = (hora < 10) ? '0' + hora : hora;
+
+    creDia.innerHTML = dia;
+    creHora.innerHTML = hora;
+    creMinuto.innerHTML = minuto;
+    creSegundo.innerHTML = segundo;
+}
+
+function diaEventoAhora () {
+    alert('Dia del Evento!!!');
+    // Todo el codigo cuando el evento llegue
+}
+
+
+
+
+
+
+  boton.addEventListener("click", () => {
+
+    Swal.fire({
+      title: '<strong>VER LA UBICACION<u></u></strong>',
+      icon: 'success',
+      html:
+        'INGRESÁ' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.7677635648342!2d-59.9888762!3d-31.227162900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b49af85305dc59%3A0x7acc4cf0a71e6467!2sParroquia%20%22Nuestra%20Sra.%20de%20la%20Merced%22%20Hernandarias!5e0!3m2!1ses-419!2sar!4v1733837107007!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        'Podes ampliar el mapa',
+        
+        
+    })
+  });
+  
+  function exito(){
+    Swal.fire({
+        title: '<strong>VER LA UBICACION<u></u></strong>',
+        icon: 'success',
+        html:
+          'INGRESÁ' +
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.3692395356925!2d-59.9922524!3d-31.2382002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b49aef84cb7fd7%3A0x4a07eb190d744b0e!2sComplejo%20La%20Quinta%20Land!5e0!3m2!1ses-419!2sar!4v1733837020193!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+          'Podes ampliar el mapa',
+          
+          
+      })
+}
