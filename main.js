e62c3f6f82883c3d3b4e33de644988c7b7c277a5
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2025-02-18 10:32:08 -0300

    tress

--- main.js
+++ main.js
@@ -71,22 +71,11 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.7677635648342!2d-59.9888762!3d-31.227162900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b49af85305dc59%3A0x7acc4cf0a71e6467!2sParroquia%20%22Nuestra%20Sra.%20de%20la%20Merced%22%20Hernandarias!5e0!3m2!1ses-419!2sar!4v1733837107007!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3132.5111731933175!2d-57.85794832467072!3d-38.267641860113244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x958510f10bd2fac3%3A0x4b137064dbb4180b!2sQuinta%20Storani!5e0!3m2!1ses!2sar!4v1739881983690!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
     })
   });
   
-  function exito(){
-    Swal.fire({
-        title: '<strong>VER LA UBICACION<u></u></strong>',
-        icon: 'success',
-        html:
-          'INGRESÁ' +
-          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.3692395356925!2d-59.9922524!3d-31.2382002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b49aef84cb7fd7%3A0x4a07eb190d744b0e!2sComplejo%20La%20Quinta%20Land!5e0!3m2!1ses-419!2sar!4v1733837020193!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
-          'Podes ampliar el mapa',
-          
-          
-      })
-}
+ 
